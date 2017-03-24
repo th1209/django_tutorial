44f10250d4ca4fb1570ed4a048a861b36941430a
Toshiki Hata <t.hata@oratta.net>
2017-03-24 21:43:47 +0900

    index, detail用のビューとテンプレート作成

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from polls import views
 
+app_name = 'polls'
 urlpatterns = [
+    # 正規表現上の()で括った箇所はキャプチャされ、viewsのメソッドに引数として渡される。
+    # また、?P<question_id>の部分が特異かと思うが、これはdjangoの名前付きパターンと呼ばれるものである(以下に構文を示す)。
+    # (?P<name>pattern)
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
