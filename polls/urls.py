5ec6b4728a3a76748ef0f87935e1aee4967ebfcd
Toshiki Hata <t.hata@oratta.net>
2017-03-25 16:23:32 +0900

    いくつかのビューの処理を、汎用ビューにまとめた

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,25 @@
 from django.conf.urls import url
 from polls import views
 
+
+# アプリ名。複数アプリを区別する場合は必須。
+# 単一アプリしか使わない場合でも、付けておいたほうが無難だろう。
 app_name = 'polls'
 urlpatterns = [
-    # 正規表現上の()で括った箇所はキャプチャされ、viewsのメソッドに引数として渡される。
-    # また、?P<question_id>の部分が特異かと思うが、これはdjangoの名前付きパターンと呼ばれるものである(以下に構文を示す)。
-    # (?P<name>pattern)
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # 以下は、汎用ビューを使う場合のURL対応表。
+    # 以下のポイントに注目。
+    # * 汎用ビューでDBモデルを引く場合、必ず?P<pk>という名前付きパターンを使うこと。
+    # * 対応するビューの関数名は、'(クラス名.as_view())'で指定。
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+
+    # * 以下コメントアウトは、汎用ビューを使う以前のurl対応表。
+    # * 正規表現上の()で括った箇所はキャプチャされ、viewsのメソッドに引数として渡される。
+    #   また、?P<question_id>の部分が特異かと思うが、これはdjangoの名前付きパターンと呼ばれるものである(以下に構文を示す)。
+    #   (?P<name>pattern)
+    # url(r'^$', views.index, name='index'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,72 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.template import loader
+# from django.template import loader
+from django.views import generic
 from polls.models import Question
 from polls.models import Choice
 
 
-def index(request):
-    """全てのQuestionモデルの一覧。"""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+# 以下のいくつかのクラスは、汎用ビューと呼ばれる、よく使われる処理をクラス化したもの。
+# 使いこなすにはもう少し勉強が必要そうなので、要調査。以下のQiitaの記事が導入に易しそう。
+# [Djangoにおけるクラスベース汎用ビューの入門と使い方サンプル](http://qiita.com/felyce/items/7d0187485cad4418c073)
+
+# 以下はListViewを継承した汎用ビュー。
+# ListViewは、あるオブジェクトの一覧を表示するという汎用的な役割を持つ。
+class IndexView(generic.ListView):
+    # 汎用ビューでは、ビューがどのDBモデルを扱うのか知る必要がある。
+    # 汎用ビューにこれを教えるには、以下のmodelプロパティを定義することで行う。
+    #model = Question
+    # 汎用ビューでは、決められたテンプレート名を使う。
+    # ListViewの場合は、'(app name)/(model name)_list.html'という形式。
+    # 以下のtemplate_nameプロパティを定義すると、代わりにプロパティで指定したテンプレートを作ってくれる。
+    template_name = 'polls/index.html'
+    # ListViewでは、'(model name)_list'という名称のオブジェクト配列変数が自動的に生成される。
+    # 以下ではその変数名を上書いている。
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """このメソッドをオーバーライドすることで、
+        取得するDBモデルリストの結果を上書きできる。
+        """
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    """特定Questionの詳細。"""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    # DetailViewの場合は、'(app name)/(model name)_detail.html'という形式のテンプレートを使う。
+    # ここでも、template_nameプロパティを定義することで、挙動を上書いている。
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# 以下コメントアウトは、汎用ビューを定義する前のビュー関数。
+
+# def index(request):
+#     """全てのQuestionモデルの一覧。"""
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
+# def detail(request, question_id):
+#     """特定Questionの詳細。"""
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+# def results(request, question_id):
+#     """投票結果。"""
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -41,9 +86,3 @@ def vote(request, question_id):
         # POST下データなので、リダイレクトする
         # また、以下のreverse関数はURLの逆引きと呼ばれるもので、URLに付けた名称から、対応するviewのメソッドを呼び出す際に使う。
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def results(request, question_id):
-    """投票結果。"""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
